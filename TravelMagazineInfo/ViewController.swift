9c915e11d6c9688c8c06179aa3172578a6cab09f
yoonwooiOS <yoonwoo4429@kakao.com>
2024-05-25 17:25:42 +0900

    Initial Commit

---
+++ TravelMagazineInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelMagazineInfo
+//
+//  Created by 김윤우 on 5/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
